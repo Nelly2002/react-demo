873d1cdf6f6e93e2ba3b3ac490cb65a9577b4140
Nelly2002 <nellymadanyan@mail.ru>
2020-05-13 23:38:16 +0400

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-//import Person from './components/Person';
-// import {Hello} from './tools'
-//import * as newObj from './tools';
+/* import Person from './components/Person';
+import {Hello} from './tools'
+import * as newObj from './tools';
 import User from './components/User';
+import Animal from './components/Animal'; */
+import Input from './components/Input';
+
 
 // newObj.getThis();
 
 function App() {
-  const surname = 'Gates';
+  // const surname = 'Gates';
   return (
     <div className="App">
-      <User name = "Bill" surname = {surname}/>
+    {/*   <User name = "Bill" surname = {surname}/>
       <User name = "John" surname = 'Doe'/>
       <User name = "Mark" surname = 'Clark'/>
-
+      <Animal type='dog' age={0}/> */} 
+      <Input/>
     </div>
   );
 }
